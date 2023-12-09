4e351f020cdccedfa58b306f7c00a9a877c3f5ba
user <user@localhost.localdomain>
2023-12-09 13:39:01 +0300

    home page and register component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
 
 <script>
 import { defineComponent } from 'vue'
+import axios from 'axios'
+
+const client = axios.create({
+    baseURL: process.env.BACKEND_URL
+});
 
 export default defineComponent({
   name: 'App'
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+<div style="margin: auto; width: 50%; margin-top: 100px"> Home page. Ater login, you can proceed to YOUR CATS or CHAT </div>
+</template>
