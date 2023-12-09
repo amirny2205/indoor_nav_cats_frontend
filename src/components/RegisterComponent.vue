d2ec7e18d53becb556f90f19f48c4bcb8a99093b
user <user@localhost.localdomain>
2023-12-09 14:37:57 +0300

    register basic functionality

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -1,11 +1,26 @@
-  <script setup>
+<script setup>
+  import axios from 'axios'
+
+  function register(username, password) {
+    axios.post(import.meta.env.VITE_BACKEND_URL + '/' + 'userauth' + '/' + 'register' + '/',
+	       {username: 'username', password: 'password'},
+	       { headers: {
+		 'Access-Control-Allow-Origin': '*',
+		 'Content-type': 'application/json'}
+	       }
+	      ).then(function (response) {
+		 console.log(response)
+	       }
+		       )
+  }
+		      
 </script>
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-input filled v-model="text" label="username" />
-    <q-input filled v-model="text" label="password" />
+    <q-input filled v-model="username" label="username" />
+    <q-input filled v-model="password" label="password" />
     <q-btn color="white" text-color="black" label="login" />
-    <q-btn color="white" text-color="black" label="register" />
+    <q-btn @click="register" color="white" text-color="black" label="register" />
     <q-btn color="white" text-color="black" label="quit" />
   </div>
 </template>
