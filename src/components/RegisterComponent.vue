e82fd2e9cfceaedd63e5a423baa4bc6a7981febb
user <user@localhost.localdomain>
2023-12-09 16:07:34 +0300

    registration works

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -1,17 +1,29 @@
 <script setup>
   import axios from 'axios'
+import { ref } from 'vue'
 
-  function register(username, password) {
+const username = ref('')
+const password = ref('')
+  const register_dialog = ref(false)
+const registration_answer = ref('')
+
+  function register() {
     axios.post(import.meta.env.VITE_BACKEND_URL + '/' + 'userauth' + '/' + 'register' + '/',
-	       {username: 'username', password: 'password'},
+	       {username: username.value, password: password.value},
 	       { headers: {
 		 'Access-Control-Allow-Origin': '*',
 		 'Content-type': 'application/json'}
 	       }
 	      ).then(function (response) {
-		 console.log(response)
-	       }
-		       )
+		register_dialog.value = true
+		registration_answer.value = response
+  	      }
+		    ).catch(function (response) {
+		register_dialog.value = true
+		registration_answer.value = response
+  	      }
+		     )
+		    
   }
 		      
 </script>
@@ -23,4 +35,20 @@
     <q-btn @click="register" color="white" text-color="black" label="register" />
     <q-btn color="white" text-color="black" label="quit" />
   </div>
+
+   <q-dialog v-model="register_dialog">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">response</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+  {{ registration_answer }}
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 </template>
