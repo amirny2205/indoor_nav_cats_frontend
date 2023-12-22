9df08acd89d1676efa7129e80526b01c5173b6d0
user <user@localhost.localdomain>
2023-12-22 16:20:01 +0300

    chat works

--- src/pages/ChatsPage.vue
+++ src/pages/ChatsPage.vue
@@ -1,12 +1,15 @@
 <script setup>
 import axios from 'axios'
- import { ref } from 'vue';
+import { ref } from 'vue';
 const all_users = ref([]);
 const username = ref(localStorage.username);
 const socket = new WebSocket('ws://127.0.0.1:8000/ws');
 const current_recipient = ref('')
+const messagelist = ref([])
+const localStorageData = ref(localStorage)
+const new_message = ref('')
 
-
+ 
 socket.onopen = function (e) {
 	socket.send(JSON.stringify({
 		message: 'Hello from Js client'
@@ -15,7 +18,8 @@ socket.onopen = function (e) {
 
 socket.onmessage = function (event) {
 	try {
-		console.log(event);
+		console.log('onmessage')
+		get_message_list()
 	} catch (e) {
 		console.log('Error:', e.message);
 	}
@@ -30,53 +34,67 @@ function getAllUsers() {
 			}
 		}
 	).then(function (response) {
-	  all_users.value = response.data
+		all_users.value = response.data
 	}
 	)
 }
- getAllUsers()
- const messagelist = ref([])
- function get_message_list() {
-       axios.get(import.meta.env.VITE_BACKEND_URL + '/' + 'chat' + '/' + 'messagelist' + '/',
-	       { headers: {
-		 'Access-Control-Allow-Origin': '*',
-		 'Content-type': 'application/json'}
-	       }
-       ).then(function (response) {
-	 messagelist.value = response.data
-	 getAllUsers()
-	 messagelist.value.forEach((message) => {
-	   all_users.value.forEach((user) => {
-	     if (user['id'] == message['msg_to'] ) {
-	       message['msg_to'] = user['username']
-	     }
-	     if (user['id'] == message['msg_from'] ) {
-	       message['msg_from'] = user['username']
-	     }
-	   })
-	 })
-  	      }
-       ).catch(function (response) {
-	 console.log(response)
-  	      }
+getAllUsers()
+
+function get_message_list() {
+	axios.get(import.meta.env.VITE_BACKEND_URL + '/' + 'chat' + '/' + 'messagelist' + '/',
+		{
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-type': 'application/json'
+			}
+		}
+	).then(function (response) {
+		messagelist.value = response.data
+		getAllUsers()
+		messagelist.value.forEach((message) => {
+			all_users.value.forEach((user) => {
+				if (user['id'] == message['msg_to']) {
+					message['msg_to'] = user['username']
+				}
+				if (user['id'] == message['msg_from']) {
+					message['msg_from'] = user['username']
+				}
+			})
+		})
+	}
+	).catch(function (response) {
+		console.log(response)
+	}
 	)
 
 }
- get_message_list()
+get_message_list()
 
- const localStorageData = ref(localStorage)
+
+function new_message_send() {
+	socket.send(JSON.stringify({
+		msg_from: localStorage.username,
+		msg_to: current_recipient.value.username,
+		content: new_message.value
+	}))
+	get_message_list()
+
+}
+
+setInterval(get_message_list, 1000)
 </script>
 <template>
   <div>
     <h5 style="text-align: center; margin:auto">chat page</h5> <br>
     <q-select style="margin: 5% 10%" option-value="username" option-label="username"   filled v-model="current_recipient" :options="all_users" label="recipient" />
-    <li id="messagelist" v-for="message in messagelist" :key="message.id"  style="margin: 1% 10%; background-color: #EFEFEF" v-show="message.msg_to==localStorageData.username || message.msg_from==localStorageData.username">
-      <div v-if="message.msg_to==localStorageData.username && message.msg_from==current_recipient"> {{ message.content }} </div>
-      <div v-if="message.msg_from==localStorageData.username && message.msg_to==current_recipient" style="text-align: right"> {{ message.content }} </div>
+    <li id="messagelist" v-for="message in messagelist" :key="message.id"  style="margin: 1% 10%; background-color: #EFEFEF" v-show="message.msg_to==localStorageData.username && message.msg_from==current_recipient.username || message.msg_from==localStorageData.username && message.msg_to==current_recipient.username">
+      <div v-if="message.msg_to==localStorageData.username && message.msg_from==current_recipient.username"> {{ message.content }} </div>
+      <div v-if="message.msg_from==localStorageData.username && message.msg_to==current_recipient.username" style="text-align: right"> {{ message.content }} </div>
+
     </li>
     <div style="margin: 1% 10%">
-      <q-input label="your message here"> </q-input> <q-btn> send </q-btn>
-	        &ensp;  you are logged in as: {{ username }}
+      <q-input v-model="new_message" label="your message here"> </q-input> <q-btn @click="new_message_send()"> send </q-btn>
+      &ensp;  you are logged in as <strong> {{ username }} </strong>
     </div>
   </div>
 </template>
